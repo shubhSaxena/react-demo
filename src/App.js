6122b5637b457b72373a3ff3e63180551967b37e
shubhSaxena <shubhamsaxena04@gmail.com>
2017-05-06 21:50:32 +0530

    react router example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, Route, hashHistory } from 'react-router';
+import {Router, Route, browserHistory } from 'react-router';
 
 const Home = () => <div><h1>Home</h1></div>;
 const About = () => <div><h1>About</h1></div>;
@@ -8,7 +8,7 @@ const Contact = () => <div><h1>Contact</h1></div>;
 class App extends React.Component {
   render() {
     return(
-      <Router history={ hashHistory }>
+      <Router history={ browserHistory }>
        <Route path="/" component={Home}></Route>
        <Route path="/about" component={About}></Route>
        <Route path="/contact" component={Contact}></Route>
