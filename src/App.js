704dbb6e6b8e78cf0d24778b3d887dc196516737
shubhSaxena <shubhamsaxena04@gmail.com>
2017-05-06 19:34:02 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Router, Route, hashHistory } from 'react-router';
 
-class App extends Component {
+const Home = () => <div><h1>Home</h1></div>;
+const About = () => <div><h1>About</h1></div>;
+const Contact = () => <div><h1>Contact</h1></div>;
+
+class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return(
+      <Router history={ hashHistory }>
+       <Route path="/" component={Home}></Route>
+       <Route path="/about" component={About}></Route>
+       <Route path="/contact" component={Contact}></Route>
+      </Router>
+    )
   }
 }
 
