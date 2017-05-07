bf45870ae8b3c3e8f02696fe4c3a02980d24d9f5
shubhSaxena <shubhamsaxena04@gmail.com>
2017-05-07 17:01:02 +0530

    nested data with props.children

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 
 class App extends React.Component {
-  constructor () {
-    super ();
-    this.state = {
-      txt: 'this is the state txt'
-    }
-  }
-  update (e) {
-    this.setState({txt: e.target.value})
-  }
-  render (){
+  render () {
     return (
-      <div>
-        <input type="text" onChange={this.update.bind(this)} />
-        <h1>{this.state.txt}</h1>
-        <Widget update={this.update.bind(this)} />
-        <Widget update={this.update.bind(this)} />
-        <Widget update={this.update.bind(this)} />
-      </div>
+      <Button>I  <Heart />  React</Button>
     )
   }
 }
 
-const Widget = (props) =>
- <input type="text" onChange={props.update} />
+const Button = (props) => <button>{props.children}</button>
+
+class Heart extends React.Component {
+  render () {
+    return <span>&hearts;</span>
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+// import PropTypes from 'prop-types';
+//
+// class App extends React.Component {
+//   constructor () {
+//     super ();
+//     this.state = {
+//       txt: 'this is the state txt'
+//     }
+//   }
+//   update (e) {
+//     this.setState({txt: e.target.value})
+//   }
+//   render (){
+//     return (
+//       <div>
+//         <input type="text" onChange={this.update.bind(this)} />
+//         <h1>{this.state.txt}</h1>
+//         <Widget update={this.update.bind(this)} />
+//         <Widget update={this.update.bind(this)} />
+//         <Widget update={this.update.bind(this)} />
+//       </div>
+//     )
+//   }
+// }
+//
+// const Widget = (props) =>
+//  <input type="text" onChange={props.update} />
 
 
 //
