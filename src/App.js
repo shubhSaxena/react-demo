0fa834d8b166565fd63d783de461ccd67d26fc3d
shubhSaxena <shubhamsaxena04@gmail.com>
2017-05-07 17:14:10 +0530

    custom propType validations

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
   render () {
-    return (
-      <Button>I  <Heart />  React</Button>
-    )
+    return <Title text="asdasd"/>
   }
 }
 
-const Button = (props) => <button>{props.children}</button>
+const Title = (props) => <h1>Title: {props.text} </h1>
 
-class Heart extends React.Component {
-  render () {
-    return <span>&hearts;</span>
+Title.propTypes = {
+  text(props, propName, component){
+    if(!(propName in props)){
+      return new Error('missing ${propName}')
+    }
+    if(props[propName].length < 8){
+      return new Error('${propName} was too short')
+    }
   }
 }
+
+
+
+
+// class App extends React.Component {
+//   render () {
+//     return (
+//       <Button>I  <Heart />  React</Button>
+//     )
+//   }
+// }
+
+// const Button = (props) => <button>{props.children}</button>
+//
+// class Heart extends React.Component {
+//   render () {
+//     return <span>&hearts;</span>
+//   }
+// }
 
 
 
