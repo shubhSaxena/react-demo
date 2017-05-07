24c9e31dd96a7bf98e4709c45255b6634c7cfd9e
shubhSaxena <shubhamsaxena04@gmail.com>
2017-05-07 13:15:01 +0530

    links in router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import {Router, Route, browserHistory } from 'react-router';
+import {Router, Route, browserHistory, Link } from 'react-router';
 
-const Home = () => <div><h1>Home</h1></div>;
-const About = () => <div><h1>About</h1></div>;
-const Contact = () => <div><h1>Contact</h1></div>;
+const Home = () => <div><h1>Home</h1><Links /></div>;
+const About = () => <div><h1>About</h1><Links /></div>;
+const Contact = () => <div><h1>Contact</h1><Links /></div>;
+const Links = () =>
+<nav>
+ <Link to="/">Home</Link>
+ <Link to="/about">About</Link>
+ <Link to="/contact">Contact</Link>
+</nav>
 
 class App extends React.Component {
   render() {
