4d401e163845cad982b3a170c6381ffffc8dd353
shubhSaxena <shubhamsaxena04@gmail.com>
2017-05-08 17:11:55 +0530

    higher order components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-class App extends React.Component {
-  constructor() {
-    super ();
-    this.state = {items: []}
+const HOC = (InnerComponent) =>  class extends React.Component {
+  constructor (){
+    super();
+    this.state = {count: 0}
+  }
+  update(){
+    this.setState({count: this.state.count + 1})
   }
   componentWillMount(){
-    fetch('http://swapi.co/api/people/?format=json')
-      .then( response => response.json() )
-      .then( ({results: items}) => this.setState({items}))
+    console.log("WillMount");
   }
-  filter(e){
-    this.setState({filter: e.target.value})
+  render () {
+    return (
+      <InnerComponent
+        {...this.props}
+        {...this.state}
+        update={this.update.bind(this)}
+      />
+    )
   }
-  render(){
-    let items = this.state.items;
-    if(this.state.filter){
-      items = items.filter( item =>
-      item.name.toLowerCase()
-    .includes(this.state.filter.toLowerCase()))
-    }
+}
+
+class App extends React.Component {
+  render() {
     return (
       <div>
-      <input type="text" onChange={this.filter.bind(this)} />
-      {items.map(item =>
-        <Person key={item.name} person={item}/>)}
+        <Button>button</Button>
+        <hr/>
+        <LabelHOC>label</LabelHOC>
       </div>
     )
   }
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
+const Button = HOC((props) =>  <button onClick={props.update}>
+    {props.children} - {props.count}
+    </button>)
+
+class Label extends React.Component {
+  componentWillMount(){
+    console.log("Label Will Mount");
+  }
+  render() {
+    return (
+      <label onMouseMove={this.props.update}>{this.props.children} - {this.props.count}</label>
+    )
+  }
+}
+
+
+const LabelHOC = HOC(Label)
+
+
+
+
+
+
+
+// class App extends React.Component {
+//   constructor() {
+//     super ();
+//     this.state = {items: []}
+//   }
+//   componentWillMount(){
+//     fetch('http://swapi.co/api/people/?format=json')
+//       .then( response => response.json() )
+//       .then( ({results: items}) => this.setState({items}))
+//   }
+//   filter(e){
+//     this.setState({filter: e.target.value})
+//   }
+//   render(){
+//     let items = this.state.items;
+//     if(this.state.filter){
+//       items = items.filter( item =>
+//       item.name.toLowerCase()
+//     .includes(this.state.filter.toLowerCase()))
+//     }
+//     return (
+//       <div>
+//       <input type="text" onChange={this.filter.bind(this)} />
+//       {items.map(item =>
+//         <Person key={item.name} person={item}/>)}
+//       </div>
+//     )
+//   }
+// }
+//
+// const Person = (props) => <h4>{props.person.name}</h4>
 
 
 // import PropTypes from 'prop-type';
