3ab428e05271fc790beab897335a0e5541c02bdd
shubhSaxena <shubhamsaxena04@gmail.com>
2017-05-08 13:05:06 +0530

    manage state in life cycle component

--- src/App.js
+++ src/App.js
@@ -12,16 +12,21 @@ class App extends React.Component {
   }
   componentWillMount(){
     console.log("componentWillMount");
+    this.setState({m: 2})
   }
   componentDidMount(){
     console.log("componentDidMount");
+    // console.log(ReactDOM.findDOMNode(this));
+    this.inc = setInterval(this.update, 500);
   }
   componentWillUnmount(){
     console.log("componentWillUnmount");
+    clearInterval(this.inc);
   }
   render() {
     console.log('render');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>
+    {this.state.val * this.state.m}</button>
   }
 }
 
