79ee348bb2962cdfcda43a804480ba6d960713f7
shubhSaxena <shubhamsaxena04@gmail.com>
2017-05-11 16:45:22 +0530

    basic redux setup and use

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore} from 'redux'
 
-ReactDOM.render(
-  <App cat={5} />,
-  document.getElementById('root')
-);
+const reducer = function (state, action) {
+    if (action.type == "INC"){
+        return state+action.payload;
+    }
+    if (action.type == "DEC"){
+        return state-action.payload;
+    }
+    return state;
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+    console.log("store changed", store.getState())
+})
+
+store.dispatch({type: "INC", payload: 1})
+store.dispatch({type: "INC", payload: 2})
+store.dispatch({type: "INC", payload: 33})
+store.dispatch({type: "INC", payload: 2})
+store.dispatch({type: "DEC", payload: 1})
+store.dispatch({type: "DEC", payload: 11})
+store.dispatch({type: "DEC", payload: 13})
