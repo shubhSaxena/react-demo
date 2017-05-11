69f47a4ce6a00193659e586e9e89b65992f170f4
shubhSaxena <shubhamsaxena04@gmail.com>
2017-05-11 19:45:10 +0530

    redux async and thunk

--- src/index.js
+++ src/index.js
@@ -1,41 +1,46 @@
 import { applyMiddleware,createStore} from 'redux'
+import axios from 'axios';
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
 
+const logger = createLogger()
 
-const reducers = (initialState=0, action) => {
-    if (action.type === "INC"){
-        return initialState+1;
-    } else if (action.type === "DEC"){
-        return initialState-1;
-    } else if (action === "E")
-        throw new Error("errrorrr!!")
+const initialState = {
+    fetching: false,
+    fetched: false,
+    users: [],
+    error: null
 }
 
-const logger = (store) => (next) => (action) => {
-    console.log("action fired", action);
-    next(action);
-}
-
-const error = (store) => (next) => (action) => {
-    try {
-        next(action);
-    } catch (e) {
-        console.log("error here", e);
+const reducers = (state=initialState, action) => {
+    switch (action.type){
+        case "FETCH_USERS_START": {
+            return {...state, fetching: true}
+            break;
+        }
+        case "FETCH_USERS_ERROR": {
+            return {...state, fetching: false, error: action.payload}
+            break;
+        }
+        case "RECEIVE_USERS": {
+            return {...state, fetching: false, fetched: true, users: action.payload}
+            break;
+        }
     }
+    return state
 }
 
+const store = createStore(reducers, applyMiddleware(thunk, logger));
 
-const middleware = applyMiddleware(logger, error);
+store.dispatch((dispatch) => {
+    dispatch({type: "FETCH_USERS_START"})
+    axios.get("http://rest.learncode.academy/api/wstren/users")
+        .then((response) => {
+            dispatch({type: "RECEIVE_USERS", payload: response.data})
+        })
+        .catch((err) => {
+            dispatch({type: "FETCH_USERS_ERROR", payload: err})
+        })
+    // do something async
 
-const store = createStore(reducers, 1, middleware);
-
-store.subscribe(() => {
-    console.log("store changed", store.getState())
 })
-
-store.dispatch({type: "INC"})
-store.dispatch({type: "INC"})
-store.dispatch({type: "INC"})
-store.dispatch({type: "INC"})
-store.dispatch({type: "DEC"})
-store.dispatch({type: "DEC"})
-store.dispatch({type: "E"})
